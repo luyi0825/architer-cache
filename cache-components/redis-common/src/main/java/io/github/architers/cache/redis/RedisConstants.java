6fcc7bb44e9b98815388b71a5a0804ea97f8fd50
luyi <893431435@qq.com>
2021-08-29 11:19:25 +0800

    fix:redis常量类修改

--- cache-components/redis-common/src/main/java/io/github/architers/cache/redis/RedisConstants.java
+++ cache-components/redis-common/src/main/java/io/github/architers/cache/redis/RedisConstants.java
@@ -1,11 +1,14 @@
 package io.github.architers.cache.redis;
 
 /**
- * redis常量
+ * redis常量类
  *
  * @author luyi
  * @version 1.0.0
  */
 public class RedisConstants {
+    /**
+     * cacheName和key的分割符号
+     */
     public final static String SPLIT = ":";
 }
