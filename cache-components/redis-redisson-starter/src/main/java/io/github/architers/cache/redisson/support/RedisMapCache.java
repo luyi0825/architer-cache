1c4120d0d820c17eccf83a42cf1d2c00e6f09cb6
luyi <893431435@qq.com>
2021-08-29 17:33:51 +0800

    fix:注释无用的类

--- cache-components/redis-redisson-starter/src/main/java/io/github/architers/cache/redisson/support/RedisMapCache.java
+++ cache-components/redis-redisson-starter/src/main/java/io/github/architers/cache/redisson/support/RedisMapCache.java
@@ -1,101 +1,101 @@
-package io.github.architers.cache.redisson.support;
-
-
-import io.github.architers.cache.redis.JsonUtils;
-import io.github.architers.cache.redis.RedisCache;
-import org.redisson.api.RedissonClient;
-
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
-/**
- * @author luyi
- */
-public class RedisMapCache extends RedisCache {
-
-    private final RedissonClient client;
-
-    public RedisMapCache(String cacheName, RedissonClient client) {
-        super(cacheName);
-        this.client = client;
-    }
-
-
-    @Override
-    public void set(String key, Object value) {
-        client.getMap(cacheName).put(key, value);
-    }
-
-    @Override
-    public void set(Map<String, Object> map) {
-        client.getMap(cacheName).putAll(map);
-    }
-
-    @Override
-    public void set(String key, Object value, long expire, TimeUnit timeUnit) {
-        client.getMap(cacheName).put(key, value);
-        client.getKeys().expire(getCacheKey(key), expire, timeUnit);
-    }
-
-    @Override
-    public boolean setIfAbsent(String key, Object value) {
-        return !(client.getMap(cacheName).putIfAbsent(key, value) == value);
-    }
-
-    @Override
-    public boolean setIfAbsent(String key, Object value, long expire, TimeUnit timeUnit) {
-        if (this.setIfAbsent(key, value)) {
-            return client.getKeys().expire(getCacheKey(key), expire, timeUnit);
-        }
-        return false;
-    }
-
-    @Override
-    public boolean setIfPresent(String key, Object value) {
-        return false;
-    }
-
-    @Override
-    public boolean setIfPresent(String key, Object value, long expire, TimeUnit timeUnit) {
-        return false;
-    }
-
-    @Override
-    public Object getAndSet(String key, Object value) {
-        return client.getMap(cacheName);
-    }
-
-    @Override
-    public Object get(String key) {
-        return client.getMap(cacheName).get(key);
-    }
-
-    @Override
-    public Collection<Object> multiGet(Set<String> keys) {
-        return client.getMap(cacheName).getAll(Collections.singleton(keys)).values();
-    }
-
-    @Override
-    public <T> T get(String key, Class<T> clazz) {
-        Object value = client.getMap(cacheName).get(key);
-        if (value instanceof String) {
-            return JsonUtils.readValue((String) value, clazz);
-        }
-        return (T) value;
-    }
-
-    @Override
-    public boolean delete(String key) {
-        return client.getMap(cacheName).remove(key) != null;
-    }
-
-    @Override
-    public long multiDelete(Collection<String> keys) {
-        return 0;
-    }
-
-    @Override
-    public Map<Object, Object> getAll() {
-        return client.getMap(cacheName).readAllMap();
-    }
-}
+//package io.github.architers.cache.redisson.support;
+//
+//
+//import io.github.architers.cache.redis.JsonUtils;
+//import io.github.architers.cache.redis.RedisCache;
+//import org.redisson.api.RedissonClient;
+//
+//import java.util.*;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * @author luyi
+// */
+//public class RedisMapCache extends RedisCache {
+//
+//    private final RedissonClient client;
+//
+//    public RedisMapCache(String cacheName, RedissonClient client) {
+//        super(cacheName);
+//        this.client = client;
+//    }
+//
+//
+//    @Override
+//    public void set(String key, Object value) {
+//        client.getMap(cacheName).put(key, value);
+//    }
+//
+//    @Override
+//    public void set(Map<String, Object> map) {
+//        client.getMap(cacheName).putAll(map);
+//    }
+//
+//    @Override
+//    public void set(String key, Object value, long expire, TimeUnit timeUnit) {
+//        client.getMap(cacheName).put(key, value);
+//        client.getKeys().expire(getCacheKey(key), expire, timeUnit);
+//    }
+//
+//    @Override
+//    public boolean setIfAbsent(String key, Object value) {
+//        return !(client.getMap(cacheName).putIfAbsent(key, value) == value);
+//    }
+//
+//    @Override
+//    public boolean setIfAbsent(String key, Object value, long expire, TimeUnit timeUnit) {
+//        if (this.setIfAbsent(key, value)) {
+//            return client.getKeys().expire(getCacheKey(key), expire, timeUnit);
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean setIfPresent(String key, Object value) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean setIfPresent(String key, Object value, long expire, TimeUnit timeUnit) {
+//        return false;
+//    }
+//
+//    @Override
+//    public Object getAndSet(String key, Object value) {
+//        return client.getMap(cacheName);
+//    }
+//
+//    @Override
+//    public Object get(String key) {
+//        return client.getMap(cacheName).get(key);
+//    }
+//
+//    @Override
+//    public Collection<Object> multiGet(Set<String> keys) {
+//        return client.getMap(cacheName).getAll(Collections.singleton(keys)).values();
+//    }
+//
+//    @Override
+//    public <T> T get(String key, Class<T> clazz) {
+//        Object value = client.getMap(cacheName).get(key);
+//        if (value instanceof String) {
+//            return JsonUtils.readValue((String) value, clazz);
+//        }
+//        return (T) value;
+//    }
+//
+//    @Override
+//    public boolean delete(String key) {
+//        return client.getMap(cacheName).remove(key) != null;
+//    }
+//
+//    @Override
+//    public long multiDelete(Collection<String> keys) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public Map<Object, Object> getAll() {
+//        return client.getMap(cacheName).readAllMap();
+//    }
+//}
