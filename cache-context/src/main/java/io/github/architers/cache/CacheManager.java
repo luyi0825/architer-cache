25f16d3ba423473876c90f81b8ad9eceddef32a6
luyi <893431435@qq.com>
2021-08-29 17:40:34 +0800

    fix:缓存管理接口类注释完善

--- cache-context/src/main/java/io/github/architers/cache/CacheManager.java
+++ cache-context/src/main/java/io/github/architers/cache/CacheManager.java
@@ -3,16 +3,24 @@ package io.github.architers.cache;
 
 /**
  * @author luyi
- * 注解缓存操作service接口层
+ * @version 1.0.0
+ * 缓存管理接口类，用于获取不同缓存的实例对象
  */
 public interface CacheManager {
     /**
-     * 得到缓存
+     * 得到简单缓存（key-value的形式）
      *
-     * @param cacheName 缓存名称
+     * @param cacheName 缓存名称，不能为空
+     * @return 简单缓存对象实例
      */
     Cache getSimpleCache(String cacheName);
 
+    /**
+     * 得到map形式的缓存
+     *
+     * @param cacheName 缓存名称，不能为空
+     * @return map缓存对象实例
+     */
     Cache getMapCache(String cacheName);
 
 }
