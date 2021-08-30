3f0bdfa86e833e2dc8e63d90da4ce83b9476b08c
luyi <893431435@qq.com>
2021-08-30 22:05:03 +0800

    fix:put代码对于过期时间的代码优化

--- cache-context/src/main/java/io/github/architers/cache/operation/PutCacheOperationHandler.java
+++ cache-context/src/main/java/io/github/architers/cache/operation/PutCacheOperationHandler.java
@@ -1,6 +1,7 @@
 package io.github.architers.cache.operation;
 
 
+import io.github.architers.cache.CacheConstants;
 import io.github.architers.cache.expression.ExpressionMetadata;
 import io.github.architers.cache.proxy.MethodReturnValueFunction;
 import io.github.architers.cache.utils.CacheUtils;
@@ -10,6 +11,7 @@ import java.util.Collection;
 
 /**
  * 对应PutCacheOperation
+ * 先调用返回结果，后put
  *
  * @author luyi
  */
@@ -29,12 +31,19 @@ public class PutCacheOperationHandler extends CacheOperationHandler {
         long expireTime = CacheUtils.getExpireTime(putCacheOperation.getExpireTime(), putCacheOperation.getRandomTime());
         String cacheValue = putCacheOperation.getCacheValue();
         methodReturnValueFunction.proceed();
+        //解析缓存值
         Object value = expressionParser.parserExpression(expressionMetadata, cacheValue);
         if (this.canHandler(operation, expressionMetadata, false)) {
             lockExecute.execute(operation.getLocked(), expressionMetadata, () -> {
                 Object key = parseCacheKey(expressionMetadata, operation.getKey());
                 for (String cacheName : cacheNames) {
-                    chooseCache(operation, cacheName).set(key, value, expireTime, putCacheOperation.getExpireTimeUnit());
+                    if (CacheConstants.NEVER_EXPIRE == expireTime) {
+                        //永不过期
+                        chooseCache(operation, cacheName).set(key, value);
+                    } else {
+                        //有过期时间
+                        chooseCache(operation, cacheName).set(key, value, expireTime, putCacheOperation.getExpireTimeUnit());
+                    }
                 }
                 return null;
             });
