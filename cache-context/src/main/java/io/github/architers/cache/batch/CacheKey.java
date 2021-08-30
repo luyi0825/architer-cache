ea2955e56fd51b9023770ecc303727cb56af3ed1
luyi <893431435@qq.com>
2021-08-30 23:33:30 +0800

    fix:修改批量缓存的key,value注解无效的bug

--- cache-context/src/main/java/io/github/architers/cache/batch/CacheKey.java
+++ cache-context/src/main/java/io/github/architers/cache/batch/CacheKey.java
@@ -1,8 +1,16 @@
 package io.github.architers.cache.batch;
 
+import io.github.architers.cache.annotation.Cacheables;
+
+import java.lang.annotation.*;
+
 /**
  * @author luyi
  * 缓存key的标识
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface CacheKey {
 }
--- cache-context/src/main/java/io/github/architers/cache/batch/CacheValue.java
+++ cache-context/src/main/java/io/github/architers/cache/batch/CacheValue.java
@@ -1,9 +1,15 @@
 package io.github.architers.cache.batch;
 
+import java.lang.annotation.*;
+
 /**
  * @author luyi
  * @version 1.0.0
  * 缓存值的标识
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface CacheValue {
 }
