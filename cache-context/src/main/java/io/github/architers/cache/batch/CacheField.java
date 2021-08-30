82fc900b4c054fea25ecf120e5ece7c18be71b07
luyi <893431435@qq.com>
2021-08-30 23:32:26 +0800

    feat:批量缓存的缓存字段

---
+++ cache-context/src/main/java/io/github/architers/cache/batch/CacheField.java
@@ -0,0 +1,34 @@
+package io.github.architers.cache.batch;
+
+/**
+ * 批量缓存的缓存字段
+ *
+ * @author luyi
+ * @version 1.0.0
+ */
+public class CacheField {
+    /**
+     * key对应的字段
+     */
+    private String key;
+    /**
+     * value对应的字段
+     */
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
