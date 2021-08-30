bbbcdc84cb33c5937e105d15f719e36c124931d9
luyi <893431435@qq.com>
2021-08-31 00:45:33 +0800

    fix:增加缓存值

--- cache-context/src/main/java/io/github/architers/cache/operation/DeleteCacheOperation.java
+++ cache-context/src/main/java/io/github/architers/cache/operation/DeleteCacheOperation.java
@@ -6,4 +6,16 @@ package io.github.architers.cache.operation;
  * @author luyi
  */
 public class DeleteCacheOperation extends BaseCacheOperation {
+    /**
+     * 缓存值
+     */
+    private String cacheValue;
+
+    public String getCacheValue() {
+        return cacheValue;
+    }
+
+    public void setCacheValue(String cacheValue) {
+        this.cacheValue = cacheValue;
+    }
 }
