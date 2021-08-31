d3989731d72198355f5edfa4484fd7350bb207c8
luyi <893431435@qq.com>
2021-08-31 20:29:58 +0800

    fix:put批量操作修改

--- cache-context/src/main/java/io/github/architers/cache/operation/PutCacheOperationHandler.java
+++ cache-context/src/main/java/io/github/architers/cache/operation/PutCacheOperationHandler.java
@@ -1,7 +1,10 @@
 package io.github.architers.cache.operation;
 
 
+import io.github.architers.cache.Cache;
 import io.github.architers.cache.CacheConstants;
+import io.github.architers.cache.CacheManager;
+import io.github.architers.cache.CacheMode;
 import io.github.architers.cache.expression.ExpressionMetadata;
 import io.github.architers.cache.proxy.MethodReturnValueFunction;
 import io.github.architers.cache.utils.CacheUtils;
@@ -37,12 +40,11 @@ public class PutCacheOperationHandler extends CacheOperationHandler {
             lockExecute.execute(operation.getLocked(), expressionMetadata, () -> {
                 Object key = parseCacheKey(expressionMetadata, operation.getKey());
                 for (String cacheName : cacheNames) {
-                    if (CacheConstants.NEVER_EXPIRE == expireTime) {
-                        //永不过期
-                        chooseCache(operation, cacheName).set(key, value);
+                    Cache cache = chooseCache(operation, cacheName);
+                    if (CacheConstants.BATCH_CACHE_KEY.equals(key)) {
+                        cache.multiSet(value, expireTime, putCacheOperation.getExpireTimeUnit());
                     } else {
-                        //有过期时间
-                        chooseCache(operation, cacheName).set(key, value, expireTime, putCacheOperation.getExpireTimeUnit());
+                        cache.set(key, value, expireTime, putCacheOperation.getExpireTimeUnit());
                     }
                 }
                 return null;
