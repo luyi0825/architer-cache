2d9de9223f2a1d977e290ab95f8106cdaafc1a97
luyi <893431435@qq.com>
2021-09-03 16:18:41 +0800

    test:缓存名称修改

--- cache-test/src/test/java/io/github/architers/cache/lock/CacheLockService.java
+++ cache-test/src/test/java/io/github/architers/cache/lock/CacheLockService.java
@@ -4,7 +4,7 @@ package io.github.architers.cache.lock;
 import java.util.concurrent.TimeUnit;
 
 public interface CacheLockService {
-    @Locked(lockName = "'test'", key = "#lockKey", tryTime = 1, timeUnit = TimeUnit.MINUTES)
+    @Locked(lockName = "'CacheLockService_test1'", key = "#lockKey", tryTime = 1, timeUnit = TimeUnit.SECONDS)
         //@Cacheable(cacheName = "'test'",key = "#lockKey",expireTime = 2)
     void test1(String lockKey);
 }
