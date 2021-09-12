538c7f8cfe70ddce7d6d9633cc03c1b757de1647
luyi <893431435@qq.com>
2021-09-12 18:01:04 +0800

    fix:考虑到cacheValue为null的情况，并完善注释

--- cache-context/src/main/java/io/github/architers/cache/operation/PutCacheOperationHandler.java
+++ cache-context/src/main/java/io/github/architers/cache/operation/PutCacheOperationHandler.java
@@ -3,20 +3,22 @@ package io.github.architers.cache.operation;
 
 import io.github.architers.cache.Cache;
 import io.github.architers.cache.CacheConstants;
-import io.github.architers.cache.CacheManager;
-import io.github.architers.cache.CacheMode;
 import io.github.architers.cache.expression.ExpressionMetadata;
 import io.github.architers.cache.proxy.MethodReturnValueFunction;
 import io.github.architers.cache.utils.CacheUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.Collection;
 
 
 /**
- * 对应PutCacheOperation
- * 先调用返回结果，后put
+ * 对应PutCacheOperation的handler,先调用返回结果，后put.
+ * <li>默认使用方法的返回值作为缓存，如果指定了使用了缓存值就用指定的缓存值</li>
+ * <li>支持批量设置缓存</li>
+ * <li>支持锁</li>
  *
  * @author luyi
+ * @version 1.0.0
  */
 public class PutCacheOperationHandler extends CacheOperationHandler {
 
@@ -33,18 +35,21 @@ public class PutCacheOperationHandler extends CacheOperationHandler {
         Collection<String> cacheNames = getCacheNames(operation, expressionMetadata);
         long expireTime = CacheUtils.getExpireTime(putCacheOperation.getExpireTime(), putCacheOperation.getRandomTime());
         String cacheValue = putCacheOperation.getCacheValue();
-        methodReturnValueFunction.proceed();
-        //解析缓存值
-        Object value = expressionParser.parserExpression(expressionMetadata, cacheValue);
+        Object value = methodReturnValueFunction.proceed();
+        //默认为方法的返回值，当设置了返回值就用指定的返回值
+        if (StringUtils.hasText(cacheValue)) {
+            value = expressionParser.parserExpression(expressionMetadata, cacheValue);
+        }
         if (this.canHandler(operation, expressionMetadata, false)) {
+            Object finalValue = value;
             lockExecute.execute(operation.getLocked(), expressionMetadata, () -> {
                 Object key = parseCacheKey(expressionMetadata, operation.getKey());
                 for (String cacheName : cacheNames) {
                     Cache cache = chooseCache(operation, cacheName);
                     if (CacheConstants.BATCH_CACHE_KEY.equals(key)) {
-                        cache.multiSet(value, expireTime, putCacheOperation.getExpireTimeUnit());
+                        cache.multiSet(finalValue, expireTime, putCacheOperation.getExpireTimeUnit());
                     } else {
-                        cache.set(key, value, expireTime, putCacheOperation.getExpireTimeUnit());
+                        cache.set(key, finalValue, expireTime, putCacheOperation.getExpireTimeUnit());
                     }
                 }
                 return null;
