7ba8ba9fed3516bf4738645621595685f48bf9c0
luyi <893431435@qq.com>
2021-09-29 09:18:04 +0800

    fix:修改测试名称

--- cache-test/src/test/java/io/github/architers/cache/mapvalue/MapValueService.java
+++ cache-test/src/test/java/io/github/architers/cache/mapvalue/MapValueService.java
@@ -8,9 +8,11 @@ import io.github.architers.cache.annotation.DeleteCache;
 
 public interface MapValueService {
 
-    @Cacheable(cacheName = "mapValue_user", key = "#userName", expireTime = 2, cacheMode = CacheMode.MAP)
+    String cacheName="mapValue_user";
+
+    @Cacheable(cacheName =MapValueService.cacheName, key = "#userName", expireTime = 2, cacheMode = CacheMode.MAP)
     UserInfo findByUserName(String userName);
 
-    @DeleteCache(cacheName = "mapValue_user", key = "#userName",cacheMode = CacheMode.MAP)
+    @DeleteCache(cacheName = MapValueService.cacheName, key = "#userName",cacheMode = CacheMode.MAP)
     void deleteByUserName(String userName);
 }
